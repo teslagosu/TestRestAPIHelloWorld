4f2020496e0fce7fd3339c42e602979451e2bb42
Sebastian Häyry <sebhyr-7@student.ltu.se>
2019-04-12 13:42:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Hello world
+adsdsadasdasda
+##satan
+asdasd
---
+++ src/main/java/com/hmanhx/REST/RestDemo/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.hmanhx.REST.RestDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String index(){
+
+        return "Hello world!";
+    }
+
+
+}
